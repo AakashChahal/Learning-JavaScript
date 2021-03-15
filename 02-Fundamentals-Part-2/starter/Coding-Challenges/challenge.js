b15544414a8aa5f9f2ad953f1aeb9e7c835beeb9
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-16 00:03:11 +0530

    Challenge #2 done

--- 02-Fundamentals-Part-2/starter/Coding-Challenges/challenge.js
+++ 02-Fundamentals-Part-2/starter/Coding-Challenges/challenge.js
@@ -10,3 +10,18 @@ const checkWinner = function (avgDolhins, avgKoalas) {
 checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
 checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
 // END //
+
+// CHALLENGE #2 //
+const calcTip = (billValue) =>
+    billValue <= 300 && billValue >= 50 ? billValue * 0.15 : billValue * 0.2;
+// console.log(calcTip(100));
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log("bills array: ");
+console.log(bills);
+console.log("tips array: ");
+console.log(tips);
+console.log("total bills array: ");
+console.log(total);
+// END //
