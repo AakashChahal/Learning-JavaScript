7440e3e81cfef526ce5d465dbf923d579bf1e113
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-17 23:10:22 +0530

    Guess the number commit #1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,32 @@
-'use strict';
-document.querySelector('.message').textContent = '⬆⬇Correct Wrong Answer';
-document.querySelector('.guess').value = 100;
+"use strict";
+// document.querySelector('.message').textContent = '⬆⬇Correct Wrong Answer';
+// document.querySelector('.guess').value = 100;
+let lives = 5;
+const answer = Math.trunc(Math.random() * 20 + 1);
+// console.log(answer);
+
+document.querySelector(".check").addEventListener("click", function () {
+    const guessValue = Number(document.querySelector(".guess").value);
+    if (lives > 1) {
+        if (!guessValue)
+            document.querySelector(".message").textContent =
+                "⛔ Enter a number(1 to 20)";
+        else if (guessValue > answer) {
+            document.querySelector(".message").textContent = "🔼 Too High";
+            lives--;
+            document.querySelector(".lives").textContent = lives;
+        } else if (guessValue < answer) {
+            document.querySelector(".message").textContent = "🔽 Too Low";
+            lives--;
+            document.querySelector(".lives").textContent = lives;
+        } else {
+            document.querySelector(".message").textContent =
+                "✅ You guessed it";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+        }
+    } else {
+        document.querySelector(".lives").textContent = 0;
+        document.querySelector(".message").textContent = "👎 You lost the game";
+    }
+});
