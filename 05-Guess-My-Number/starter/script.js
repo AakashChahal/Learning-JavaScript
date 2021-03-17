d52196e75ec058b8a13e19f397c250d4a6b7e189
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-17 23:33:39 +0530

    Challenge #1 completed

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,22 +2,24 @@
 // document.querySelector('.message').textContent = '⬆⬇Correct Wrong Answer';
 // document.querySelector('.guess').value = 100;
 let lives = 5;
-const answer = Math.trunc(Math.random() * 20 + 1);
+let answer = Math.trunc(Math.random() * 20 + 1);
+console.log(answer);
 // console.log(answer);
 
 document.querySelector(".check").addEventListener("click", function () {
     const guessValue = Number(document.querySelector(".guess").value);
+    console.log(lives);
     if (lives > 1) {
         if (!guessValue)
             document.querySelector(".message").textContent =
                 "⛔ Enter a number(1 to 20)";
         else if (guessValue > answer) {
-            document.querySelector(".message").textContent = "🔼 Too High";
             lives--;
+            document.querySelector(".message").textContent = "🔼 Too High";
             document.querySelector(".lives").textContent = lives;
         } else if (guessValue < answer) {
-            document.querySelector(".message").textContent = "🔽 Too Low";
             lives--;
+            document.querySelector(".message").textContent = "🔽 Too Low";
             document.querySelector(".lives").textContent = lives;
         } else {
             document.querySelector(".message").textContent =
@@ -26,7 +28,25 @@ document.querySelector(".check").addEventListener("click", function () {
             document.querySelector(".number").style.width = "30rem";
         }
     } else {
-        document.querySelector(".lives").textContent = 0;
-        document.querySelector(".message").textContent = "👎 You lost the game";
+        if (lives === 1 && guessValue === answer) {
+            document.querySelector(".message").textContent =
+                "✅ You guessed it";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+        } else {
+            document.querySelector(".lives").textContent = 0;
+            document.querySelector(".message").textContent =
+                "👎 You lost the game";
+        }
     }
+});
+
+document.querySelector(".again").addEventListener("click", (e) => {
+    lives = 5;
+    answer = Math.trunc(Math.random() * 20 + 1);
+    document.querySelector(".guess").value = "";
+    document.querySelector(".lives").textContent = lives;
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector("body").style.backgroundColor = "#222";
 });
