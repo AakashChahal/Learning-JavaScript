7694addf5c276a7991e6d35e7836882d0f7da1a1
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-18 21:42:54 +0530

    keyboard events and modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const modals = document.querySelectorAll(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelectorAll(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function (e) {
+    for (let i = 0; i < modals.length; i++) {
+        modals[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+    }
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener("click", function () {
+        modals[i].classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
+    btnCloseModal[i].addEventListener("click", closeModal);
+}
+
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") closeModal();
+});
