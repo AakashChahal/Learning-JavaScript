5f74b700bc2128abe45fda43c31d942ff663dcb6
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-19 16:10:27 +0530

    Initial setup

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// Elements
+const score1Element = document.querySelector('#score--0');
+const score2Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+score1Element.textContent = 0;
+score2Element.textContent = 0;
