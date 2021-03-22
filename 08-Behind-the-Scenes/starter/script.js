600a6c5f80384dbe218ffb08d851aea0c37d8341
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-22 22:21:47 +0530

    initial

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+
+// scope and scope chain
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear <= 1996 && birthYear >= 1981) {
+            var millineal = true;
+            const str = `Oh, and you are an millineal, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+            // let output = "NEW OUTPUT";
+        }
+        console.log(millineal);
+        // console.log(output);
+        // console.log(add(3, 4)); will only run without 'strict' mode (not recommended)
+    }
+    printAge();
+    return age;
+}
+
+const firstName = "Aakash";
+calcAge(1996);
+
+// Hoisting
