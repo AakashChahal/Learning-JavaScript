91c9dc6e597648cee72b800a7cfd00a9ed020914
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-31 16:40:53 +0530

    CODING CHALLENGE #2 COMPLETED

--- 09-Data-Structures-Operators/starter/Coding-Challenges/challenge.js
+++ 09-Data-Structures-Operators/starter/Coding-Challenges/challenge.js
@@ -64,3 +64,33 @@ printGoals("Davis", "Muller", "Lewandowski", "Kimmich");
 printGoals(...game.scored);
 
 console.log((team1 > team2 && "Team 1 will win") || "Team 2 will win");
+console.log(
+    "------------------------------------------------------------------"
+);
+// Coding Challenge #2
+// task #1
+for (const [i, player] of game.scored.entries()) {
+    console.log(`Goal ${i + 1}: ${player}`);
+}
+// task #2
+const odds = Object.values(game.odds);
+let avg = 0;
+for (const odd of odds) avg += odd;
+avg /= odds.length;
+console.log(`Average odds: ${avg}`);
+
+// task #3
+for (const [res, odd] of Object.entries(game.odds)) {
+    console.log(
+        `Odd of ${
+            res === "x" ? `draw: ${odd}` : `victory ${game[res]}: ${odd}`
+        }`
+    );
+}
+
+// task #4
+const scorers = {};
+for (const player of game.scored) {
+    scorers[player] ? (scorers[player] += 1) : (scorers[player] = 1);
+}
+console.log(scorers);
