429fd8c4f0a0bb7faaf1cfb00b7ca113324b1acb
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-01 21:05:31 +0530

    string final exercise

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// Data needed for a later exercise
-const flights =
-    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
-
 // Data needed for first part of the section
 
 const hours = {
@@ -361,3 +357,18 @@ console.log(msg.padStart(25, "+").padEnd(35, "+")); // adds "+" to the start of 
 // Repeat
 const msg2 = "Bad Weather!!... ";
 console.log(msg2.repeat(5));
+
+// Final string exercise
+const flights =
+    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+for (const flight of flights.split("+")) {
+    let [type, from, to, time] = flight.split(";");
+    type =
+        `${type.includes("Delayed") ? "🛑 " : ""}` +
+        type.replaceAll("_", " ").trim();
+    from = from.slice(0, 3).toUpperCase();
+    to = to.slice(0, 3).toUpperCase();
+    time = `(${time.replace(":", "h")})`;
+    console.log(`${type} from ${from} to ${to} ${time}`.padStart(45));
+}
