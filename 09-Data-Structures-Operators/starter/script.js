a8077946f4a7fd4e3fdff6a92b7e408db7ce7cd4
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-01 08:08:45 +0530

    Maps: Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -231,3 +231,28 @@ console.log(ordersSet);
 const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
 const staffUnique = [...new Set(staff)]; // converting a set to array using spread operator
 console.log(staffUnique);
+
+// Maps
+const rest = new Map();
+rest.set("name", "Dominos");
+rest.set(1, "SomePlace, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+    .set("open", 11)
+    .set("close", 23)
+    .set(true, "We are open")
+    .set("we are closed");
+// retrieving values
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get("true"));
+
+const time = 22;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+rest.delete(2);
+console.log(rest.size);
+// rest.clear();
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
