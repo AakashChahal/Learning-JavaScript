6a088fcb35c879e2812bbea58c32f699f9514cf6
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-03 11:05:39 +0530

    default parameter

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (
+    flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers
+) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 298);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+
+createBooking("LH123", undefined, 300); // way to skip a parameter
