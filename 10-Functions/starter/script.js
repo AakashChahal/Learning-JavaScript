49f92a211f7b817a2aacf239ad9f1a54c8b7d4f4
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-11 10:30:48 +0530

    Closure

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -158,3 +158,57 @@ console.log(addVAT2(29));
     console.log("This will only run once");
 })();
 (() => console.log("This will also only run once"))();
+
+// Closures
+const secureBooking = function () {
+    let passengersCount = 0;
+    return function () {
+        passengersCount++;
+        console.log(`${passengersCount} passengers`);
+    };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+console.dir(booker);
+
+// MORE closure examples
+// Example 1
+let f;
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    };
+};
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    };
+};
+
+g();
+f();
+console.dir(f);
+
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
