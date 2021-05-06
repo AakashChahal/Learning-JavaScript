d12395d87ceddcc7fcf99b6fe4d3e0cc049262ef
Aakash Chahal <aakash.chahal62@gmail.com>
2021-05-06 22:25:30 +0530

    Challenge #3 completed

--- 11-Arrays-Bankist/starter/Coding-Challenges/challenge.js
+++ 11-Arrays-Bankist/starter/Coding-Challenges/challenge.js
@@ -55,9 +55,10 @@ console.log("--------------------Coding Challenge #2-------------------");
 // § Data 2: [16, 6, 10, 5, 6, 1, 4]
 
 const calcAverageHumanAge = function (ages) {
-    const dogsHumanAges = ages
-        .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
-        .filter((age) => age >= 18);
+    const dogsHumanAges = ages.map((age) =>
+        age <= 2 ? 2 * age : 16 + age * 4
+    );
+    dogsHumanAges = dogsHumanAges.filter((age) => age >= 18);
     console.log(`Dogs ages in human years: [${dogsHumanAges}]`);
     const avgDogHumanAge =
         dogsHumanAges.reduce((acc, age) => acc + age) / dogsHumanAges.length;
@@ -68,3 +69,20 @@ console.log("------------TEST DATA #1------------");
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 console.log("------------TEST DATA #2------------");
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log("--------------------Coding Challenge #3-------------------");
+// Your Task:
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+// as an arrow function, and using chaining!
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+const calcAverageHumanAgeCopy = (ages) => {
+    const dogsHumanAges = ages
+        .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+        .filter((age) => age >= 18);
+    console.log(`Dogs ages in human years: [${dogsHumanAges}]`);
+    const avgDogHumanAge =
+        dogsHumanAges.reduce((acc, age) => acc + age) / dogsHumanAges.length;
+    console.log(`Average Dogs age in Human Years: ${avgDogHumanAge}`);
+};
