3ac7649ca5b9ad9fcc9d44b8352d981201500440
Aakash Chahal <aakash.chahal62@gmail.com>
2021-10-15 18:38:36 +0530

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* contructor function and new operator */
 const Person = function (firstName, birthYear) {
     console.log("Before initializing instance properties and methods: ", this);
     // instance properties
@@ -8,17 +9,26 @@ const Person = function (firstName, birthYear) {
 
     // instance methods
     // creating functions inside constructor function isn't recommended and should be avoided
-    this.calcAge = function () {
-        console.log(2037 - this.birthYear);
-    };
-    console.log("After initializing instance propertiesand methods: ", this);
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // };
+    // console.log("After initializing instance propertiesand methods: ", this);
 };
 
 const aakash = new Person("Aakash", 2000);
 console.log(aakash);
-aakash.calcAge();
-/* steps that take place when we call a constructor function */
-// 1. New {} (empty Object) is created
-// 2. function is called, [this = {}]
-// 3. {} linked to prototype
-// 4. function automatically returns a new object
+/* 
+    steps that take place when we call a constructor function
+    // 1. New {} (empty Object) is created
+    // 2. function is called, [this = {}]
+    // 3. {} ['this'] linked to prototype
+    // 4. function automatically returns {} ['this']
+*/
+
+/* Prototypes */
+// we can add methods to constructor function using prototype, and it is a better way to add functions to any constructor function
+Person.prototype.calcAge = function () {
+    console.log(2021 - this.birthYear);
+};
+
+console.log(Person.prototype);
