0f74c7d9d1553e36e0a4cbfa29511dd826fad3ac
Aakash Chahal <aakash.chahal62@gmail.com>
2021-10-16 15:12:32 +0530

    challenge #1 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,53 +1,56 @@
 "use strict";
 
 /* contructor function and new operator */
-const Person = function (firstName, birthYear) {
-    console.log("Before initializing instance properties and methods: ", this);
-    // instance properties
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//     console.log("Before initializing instance properties and methods: ", this);
+//     // instance properties
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
 
-    // instance methods
-    // creating functions inside constructor function isn't recommended and should be avoided
-    // this.calcAge = function () {
-    //     console.log(2037 - this.birthYear);
-    // };
-    // console.log("After initializing instance propertiesand methods: ", this);
-};
+//     // instance methods
+//     // creating functions inside constructor function isn't recommended and should be avoided
+//     // this.calcAge = function () {
+//     //     console.log(2037 - this.birthYear);
+//     // };
+//     // console.log("After initializing instance propertiesand methods: ", this);
+// };
 
-const aakash = new Person("Aakash", 2000);
-console.log(aakash);
-/* 
-    steps that take place when we call a constructor function
-    // 1. New {} (empty Object) is created
-    // 2. function is called, [this = {}]
-    // 3. {} ['this'] linked to prototype
-    // 4. function automatically returns {} ['this']
-*/
+// const aakash = new Person("Aakash", 2000);
+// console.log(aakash);
+// /*
+//     steps that take place when we call a constructor function
+//     // 1. New {} (empty Object) is created
+//     // 2. function is called, [this = {}]
+//     // 3. {} ['this'] linked to prototype
+//     // 4. function automatically returns {} ['this']
+// */
 
-/* Prototypes */
-// we can add methods to constructor function using prototype, and it is a better way to add functions to any constructor function
-Person.prototype.calcAge = function () {
-    console.log(2021 - this.birthYear);
-};
+// /* Prototypes */
+// // we can add methods to constructor function using prototype, and it is a better way to add functions to any constructor function
+// Person.prototype.calcAge = function () {
+//     console.log(2021 - this.birthYear);
+// };
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-console.log(aakash.__proto__);
-console.log(aakash.__proto__ === Person.prototype);
+// console.log(aakash.__proto__);
+// console.log(aakash.__proto__ === Person.prototype);
 
-Person.prototype.species = "Homo Sapien";
-console.log(
-    "firstName is a property of aakash: ",
-    aakash.hasOwnProperty("firstName")
-);
-console.log(
-    "species is a property of aakash: ",
-    aakash.hasOwnProperty("species")
-);
+// Person.prototype.species = "Homo Sapien";
+// console.log(
+//     "firstName is a property of aakash: ",
+//     aakash.hasOwnProperty("firstName")
+// );
+// console.log(
+//     "species is a property of aakash: ",
+//     aakash.hasOwnProperty("species")
+// );
 
-console.log(aakash.__proto__.__proto__);
-console.log(Object.prototype);
+// console.log(aakash.__proto__.__proto__);
+// console.log(Object.prototype);
+
+// const h1 = document.querySelector("h1");
+// console.dir(h1);
 
 /*
     NOTE :
@@ -55,5 +58,53 @@ console.log(Object.prototype);
     i.e, Person.prototype === aakash.__proto__
 */
 
-const h1 = document.querySelector("h1");
-console.dir(h1);
+/* 
+    CHALLENGE #1
+    Tasks:
+    1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
+    'speed' property. The 'speed' property is the current speed of the car in 
+    km/h
+    2. Implement an 'accelerate' method that will increase the car's speed by 10, 
+    and log the new speed to the console
+    3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
+    the new speed to the console
+    4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
+    'brake' multiple times on each of them
+
+    Test data:
+    § Data car 1: 'BMW' going at 120 km/h
+    § Data car 2: 'Mercedes' going at 95 km/h 
+*/
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(
+        `${this.make}'s new speed after acceleration is ${this.speed} km/h`
+    );
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(
+        `${this.make}'s new speed after applying brake is ${this.speed} km/h`
+    );
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+
+car1.accelerate();
+car2.accelerate();
+car2.accelerate();
+car2.accelerate();
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+car1.accelerate();
+car2.brake();
+car2.accelerate();
+car1.accelerate();
