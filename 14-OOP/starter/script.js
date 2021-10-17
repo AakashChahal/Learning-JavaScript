4340eda0b4f8ce8f53c60868833cec51f1a6dc79
Aakash Chahal <aakash.chahal62@gmail.com>
2021-10-17 15:37:35 +0530

    separate file for challenges and notes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,61 +1,61 @@
 "use strict";
 
 /* contructor function and new operator */
-// const Person = function (firstName, birthYear) {
-//     console.log("Before initializing instance properties and methods: ", this);
-//     // instance properties
-//     this.firstName = firstName;
-//     this.birthYear = birthYear;
+const Person = function (firstName, birthYear) {
+    console.log("Before initializing instance properties and methods: ", this);
+    // instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
 
-//     // instance methods
-//     // creating functions inside constructor function isn't recommended and should be avoided
-//     // this.calcAge = function () {
-//     //     console.log(2037 - this.birthYear);
-//     // };
-//     // console.log("After initializing instance propertiesand methods: ", this);
-// };
+    // instance methods
+    // creating functions inside constructor function isn't recommended and should be avoided
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // };
+    // console.log("After initializing instance propertiesand methods: ", this);
+};
 
-// Static function function for the constructor function
-// Person.hey = function () {
-//     console.log("this is a static function");
-// };
+// Static function for the constructor function
+Person.hey = function () {
+    console.log("this is a static function");
+};
 
-// const aakash = new Person("Aakash", 2000);
-// console.log(aakash);
-// /*
-//     steps that take place when we call a constructor function
-//     // 1. New {} (empty Object) is created
-//     // 2. function is called, [this = {}]
-//     // 3. {} ['this'] linked to prototype
-//     // 4. function automatically returns {} ['this']
-// */
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+/*
+    steps that take place when we call a constructor function
+    // 1. New {} (empty Object) is created
+    // 2. function is called, [this = {}]
+    // 3. {} ['this'] linked to prototype
+    // 4. function automatically returns {} ['this']
+*/
 
-// /* Prototypes */
-// // we can add methods to constructor function using prototype, and it is a better way to add functions to any constructor function
-// Person.prototype.calcAge = function () {
-//     console.log(2021 - this.birthYear);
-// };
+/* Prototypes */
+// we can add methods to constructor function using prototype, and it is a better way to add functions to any constructor function
+Person.prototype.calcAge = function () {
+    console.log(2021 - this.birthYear);
+};
 
-// console.log(Person.prototype);
+console.log(Person.prototype);
 
-// console.log(aakash.__proto__);
-// console.log(aakash.__proto__ === Person.prototype);
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
 
-// Person.prototype.species = "Homo Sapien";
-// console.log(
-//     "firstName is a property of aakash: ",
-//     aakash.hasOwnProperty("firstName")
-// );
-// console.log(
-//     "species is a property of aakash: ",
-//     aakash.hasOwnProperty("species")
-// );
+Person.prototype.species = "Homo Sapien";
+console.log(
+    "firstName is a property of jonas: ",
+    jonas.hasOwnProperty("firstName")
+);
+console.log(
+    "species is a property of jonas: ",
+    jonas.hasOwnProperty("species")
+);
 
-// console.log(aakash.__proto__.__proto__);
-// console.log(Object.prototype);
+console.log(jonas.__proto__.__proto__);
+console.log(Object.prototype);
 
-// const h1 = document.querySelector("h1");
-// console.dir(h1);
+const h1 = document.querySelector("h1");
+console.dir(h1);
 
 /*
     NOTE :
@@ -63,280 +63,123 @@
     i.e, Person.prototype === aakash.__proto__
 */
 
-/* 
-    CHALLENGE #1
-    Tasks:
-    1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
-    'speed' property. The 'speed' property is the current speed of the car in 
-    km/h
-    2. Implement an 'accelerate' method that will increase the car's speed by 10, 
-    and log the new speed to the console
-    3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
-    the new speed to the console
-    4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
-    'brake' multiple times on each of them
-
-    Test data:
-    § Data car 1: 'BMW' going at 120 km/h
-    § Data car 2: 'Mercedes' going at 95 km/h 
-*/
-console.log("------------------ CHALLENGE #1 ------------------");
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
-};
-
-Car.prototype.accelerate = function () {
-    this.speed += 10;
-    console.log(
-        `${this.make}'s new speed after acceleration is ${this.speed} km/h`
-    );
-};
-
-Car.prototype.brake = function () {
-    this.speed -= 5;
-    console.log(
-        `${this.make}'s new speed after applying brake is ${this.speed} km/h`
-    );
-};
-
-const car1 = new Car("BMW", 120);
-const car2 = new Car("Mercedes", 95);
-
-car1.accelerate();
-car2.accelerate();
-car2.accelerate();
-car2.accelerate();
-car1.accelerate();
-car1.brake();
-car1.accelerate();
-car1.accelerate();
-car2.brake();
-car2.accelerate();
-car1.accelerate();
-
-// /* ES6 Classes */
-// class Person {
-//     constructor(fullName, birthYear) {
-//         this.fullName = fullName;
-//         this.birthYear = birthYear;
-//     }
-
-//     // this is same as adding methods using .prototype for a constructor function
-//     calcAge() {
-//         console.log(2021 - this.birthYear);
-//     }
-
-//     get age() {
-//         return 2021 - this.birthYear;
-//     }
-
-//     // Set a property that already exists
-//     set fullName(name) {
-//         if (name.includes(" ")) this._fullName = name;
-//         else alert(`${name} is not a full Name`);
-//     }
-
-//     get fullName() {
-//         return this._fullName;
-//     }
-
-//     // Static functions (these aren't added to .prototype, and can't be used by Objects)
-//     static hey() {
-//         console.log("this is a static function");
-//     }
-// }
-
-// Person.hey();
-
-// const aakash = new Person("Aakash Chahal", 2000);
-// console.log(aakash);
-// // aakash.calcAge();
-// // getting age using getter
-// console.log(aakash.age);
-// console.log(aakash.fullName);
-
-// console.log(aakash.__proto__ === Person.prototype);
-
-// // 1. Classes are not hoisted
-// // 2. Classes are also first-class citizen
-// // 3. Classes are always executed in strict mode
-
-// /* Setters and Getters */
-// const acc = {
-//     owner: "Aakash",
-//     movements: [123, 811, 9000, 150],
-
-//     // getter
-//     get latest() {
-//         return this.movements.slice(-1).pop();
-//     },
-
-//     // setter
-//     set latest(mov) {
-//         this.movements.push(mov);
-//     },
-// };
-// // setters and getters works the same way in classes as well, shown in the above class example
-
-// console.log(acc.latest);
-
-// acc.latest = 200;
-// console.log(acc.movements);
-// console.log(acc.latest);
-
-// /* Object.create */
-// // re-creating the Person class from above
-// const PersonProto = {
-//     calcAge() {
-//         console.log(2021 - this.birthYear);
-//     },
-
-//     init(firstName, birthYear) {
-//         this.firstName = firstName;
-//         this.birthYear = birthYear;
-//     },
-// };
-
-// const adam = Object.create(PersonProto);
-// const eve = Object.create(PersonProto);
-// console.log(adam);
-// console.log(adam.__proto__);
-
-// adam.init("Adam", 1900);
-// eve.init("Eve", 1900);
-// console.log(adam, eve);
-// console.log(adam.firstName, eve.firstName);
-
-/* 
-    CHALLENGE #2
-    Tasks:
-    1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
-    2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide 
-    by 1.6)
-    3. Add a setter called 'speedUS' which sets the current speed in mi/h (but 
-    converts it to km/h before storing the value, by multiplying the input by 1.6)
-    4. Create a new car and experiment with the 'accelerate' and 'brake'
-    methods, and with the getter and setter.
-
-    Test data:
-    § Data car 1: 'Ford' going at 120 km/h
-*/
-console.log("------------------ CHALLENGE #2 ------------------");
-class CarCl {
-    constructor(make, speed) {
-        this.make = make;
-        this.speed = speed;
+/* ES6 Classes */
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
     }
 
-    accelerate() {
-        this.speed += 10;
-        console.log(
-            `${this.make}'s new speed after acceleration is ${this.speed} km/h`
-        );
+    // this is same as adding methods using .prototype for a constructor function
+    calcAge() {
+        console.log(2021 - this.birthYear);
     }
 
-    brake() {
-        this.speed -= 5;
-        console.log(
-            `${this.make}'s new speed after applying brake is ${this.speed} km/h`
-        );
+    get age() {
+        return 2021 - this.birthYear;
     }
 
-    get speedUS() {
-        return this.speed / 1.6;
+    // Set a property that already exists
+    set fullName(name) {
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is not a full Name`);
     }
 
-    set speedUS(speed) {
-        this.speed = speed * 1.6;
+    get fullName() {
+        return this._fullName;
+    }
+
+    // Static functions (these aren't added to .prototype, and can't be used by Objects)
+    static hey() {
+        console.log("this is a static function");
     }
 }
 
-const ford = new CarCl("Ford", 120);
-console.log("Current speed in mi/h: ", ford.speedUS);
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.accelerate();
-console.log("updated speed in km/h: ", ford.speed);
-ford.speedUS = 50;
-console.log("new speed: ", ford.speed);
-ford.accelerate();
-console.log("updated speed in km/h: ", ford.speed);
-console.log("updated speed in mi/h: ", ford.speedUS);
+PersonCl.hey();
+
+const aakash = new PersonCl("Aakash Chahal", 2000);
+console.log(aakash);
+// aakash.calcAge();
+// getting age using getter
+console.log(aakash.age);
+console.log(aakash.fullName);
+
+console.log(aakash.__proto__ === PersonCl.prototype);
+
+// 1. Classes are not hoisted
+// 2. Classes are also first-class citizen
+// 3. Classes are always executed in strict mode
+
+/* Setters and Getters */
+const acc = {
+    owner: "Aakash",
+    movements: [123, 811, 9000, 150],
+
+    // getter
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    // setter
+    set latest(mov) {
+        this.movements.push(mov);
+    },
+};
+// setters and getters works the same way in classes as well, shown in the above class example
+
+console.log(acc.latest);
+
+acc.latest = 200;
+console.log(acc.movements);
+console.log(acc.latest);
+
+/* Object.create */
+// re-creating the Person class from above
+const PersonProto = {
+    calcAge() {
+        console.log(2021 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const adam = Object.create(PersonProto);
+const eve = Object.create(PersonProto);
+console.log(adam);
+console.log(adam.__proto__);
+
+adam.init("Adam", 1900);
+eve.init("Eve", 1900);
+console.log(adam, eve);
+console.log(adam.firstName, eve.firstName);
 
 /* Implementing Inheritance between "Classes" */
 // 1. Constructor functions
-// const Student = function (firstName, birthYear, course) {
-//     Person.call(this, firstName, birthYear);
-//     this.course = course;
-// };
-
-// // linking prototypes
-// Student.prototype = Object.create(Person.prototype);
-// Student.prototype.constructor = Student;
-
-// Student.prototype.introduce = function () {
-//     console.log(`My name is ${this.firstName} and I study ${this.course}`);
-// };
-
-// const mike = new Student("Mike", 2001, "Computer Science");
-// mike.introduce();
-// mike.calcAge();
-
-// console.log(mike.__proto__); // Student.prototype (non-inherited properties)
-// console.log(mike.__proto__.__proto__); // Student.prototype (all the inherited properties from Person.prototype)
-// console.log(mike.__proto__.__proto__.__proto__); // Object.prototype
-// console.log(mike.__proto__.__proto__.__proto__.__proto__); // null
-
-// console.log(mike instanceof Student);
-// console.log(mike instanceof Person);
-// console.log(mike instanceof Object);
-
-/* 
-    CHALLENGE #3
-    Tasks:
-    1. Use a constructor function to implement an Electric Car (called 'EV') as a child
-    "class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
-    current battery charge in % ('charge' property)
-    2. Implement a 'chargeBattery' method which takes an argument 
-    'chargeTo' and sets the battery charge to 'chargeTo'
-    3. Implement an 'accelerate' method that will increase the car's speed by 20, 
-    and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
-    km/h, with a charge of 22%'
-    4. Create an electric car object and experiment with calling 'accelerate', 
-    'brake' and 'chargeBattery' (charge to 90%). Notice what happens when 
-    you 'accelerate'! Hint: Review the definiton of polymorphism �
-    
-    Test data:
-    § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
-*/
-
-console.log("------------------ CHALLENGE #3 ------------------");
-const EV = function (make, speed, currBattery) {
-    Car.call(this, make, speed);
-    this.currBattery = currBattery;
+const Student = function (firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
 };
 
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.constructor = EV;
+// linking prototypes
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
 
-EV.prototype.chargeBattery = function (chargeTo) {
-    this.currBattery = chargeTo;
-    console.log(`${this.make}'s battery charged to ${chargeTo}%`);
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
-EV.prototype.accelerate = function () {
-    this.speed += 20;
-    this.currBattery -= 1;
-    console.log(
-        `${this.make} going at ${this.speed} km/h, with a charge of ${this.currBattery}%`
-    );
-};
+const mike = new Student("Mike", 2001, "Computer Science");
+mike.introduce();
+mike.calcAge();
 
-const tesla = new EV("Tesla", 120, 23);
+console.log(mike.__proto__); // Student.prototype (non-inherited properties)
+console.log(mike.__proto__.__proto__); // Student.prototype (all the inherited properties from Person.prototype)
+console.log(mike.__proto__.__proto__.__proto__); // Object.prototype
+console.log(mike.__proto__.__proto__.__proto__.__proto__); // null
 
-tesla.accelerate();
-tesla.brake();
-tesla.chargeBattery(90);
-tesla.accelerate();
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
