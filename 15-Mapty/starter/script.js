f63db033f5b37043b5f9a53bdcac38a836c403d4
Aakash Chahal <aakash.chahal62@gmail.com>
2021-10-29 17:35:34 +0530

    using leaflet to display map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (pos) {
+    // console.dir(pos.coords);
+    const { latitude, longitude } = pos.coords;
+    const coords = [latitude, longitude];
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    console.log('Unable to get your position');
+  }
+);
