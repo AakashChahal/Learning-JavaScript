f8b049c4d9234ebb80b94a795b700a20e37f1739
Aakash Chahal <aakash.chahal62@gmail.com>
2021-12-08 16:50:53 +0530

    using XMLHttpRequest for AJAX

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,45 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+    const req = new XMLHttpRequest();
+    req.open("GET", `https://restcountries.com/v2/name/${country}`);
+    req.send();
+
+    req.addEventListener("load", function () {
+        // console.log(this.responseText);
+
+        const [data] = JSON.parse(this.responseText);
+        // console.log(data);
+
+        const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><span>👫</span>${(
+                        +data.population / 1000000
+                    ).toFixed(2)} M </p>
+                    <p class="country__row"><span>🗣️</span>${
+                        data.languages[0].name
+                    }</p>
+                    <p class="country__row"><span>💰</span>${
+                        data.currencies[0].name
+                    }</p>
+            </div>
+        </article>
+        `;
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+// these may appear in different order, because data arrives at different time
+getCountryData("Ireland");
+getCountryData("United Kingdom");
+getCountryData("usa");
