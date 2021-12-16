a717d46cab1c7b53beec5b958b917dcee62f69c5
Aakash Chahal <aakash.chahal62@gmail.com>
2021-12-17 00:05:43 +0530

    exprting and importing in ES6 modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,21 @@
 // Importing module
-import "./shoppingCart.js";
+// import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 console.log("importing module");
+// addToCart("Bread", 5);
+// console.log(price, tq);
+
+//? importing everything from the exporting module
+// import * as ShoppingCart from "./shoppingCart.js";
+// ShoppingCart.addToCart("Bread", 5);
+// console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
+
+//? importing the default export
+import add from "./shoppingCart.js";
+add("Pizza", 3);
+
+//! imports are live connection, not copies
+import { testArr, showTestArr } from "./shoppingCart.js";
+console.log(testArr);
+testArr.pop();
+testArr.push("new", "elements", "added");
+showTestArr();
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,2 +1,30 @@
 // Exporting module
 console.log("Exporting module");
+
+//? All top-level (global) variables are scoped to this module only
+const shippingCost = 10;
+const cart = [];
+export const testArr = [10, 20, "old last value"];
+export const showTestArr = function () {
+    console.log(testArr);
+};
+
+//? Two types of exports
+//! exports must always be at the top-level (global)
+//*  1. named export
+export const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+};
+//* 1.1 exporting multiple items (using named exports)
+const totalPrice = 280;
+const totalQty = 19;
+
+export { totalPrice, totalQty as tq };
+
+//* 2. default export: used when only want to export one thing from the module
+export default function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+    console.log(cart);
+}
