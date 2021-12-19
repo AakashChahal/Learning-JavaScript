8dfd18b898d64a91701ab6a4b974d20daffc496c
Aakash Chahal <aakash.chahal62@gmail.com>
2021-12-19 19:33:10 +0530

    npm init

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -81,20 +81,20 @@ const { addToCart } = require("./shoppingCart.js")
 
 import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
 
-// const state = {
-//     cart: [
-//         { product: "bread", quantity: 5 },
-//         { product: "pizza", quantity: 5 },
-//     ],
-//     user: { loggedIn: true },
-// };
+const state = {
+    cart: [
+        { product: "bread", quantity: 5 },
+        { product: "pizza", quantity: 5 },
+    ],
+    user: { loggedIn: true },
+};
 
-// const stateClone = Object.assign({}, state);
-// const stateDeepClone = cloneDeep(state);
-// state.user.loggedIn = false;
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
 
-// console.log(stateClone);
-// console.log(stateDeepClone);
+console.log(stateClone);
+console.log(stateDeepClone);
 
 // if (module.hot) {
 //     module.hot.accept();
