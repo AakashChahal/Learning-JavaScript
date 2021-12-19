63b3c25310fcbe73e4bccaefd49dd55317762d36
Aakash Chahal <aakash.chahal62@gmail.com>
2021-12-19 16:57:36 +0530

    using npm

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -78,3 +78,20 @@ const { addToCart } = require("./shoppingCart.js")
 
 ! "export" and "require" are keywords available in node
 */
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+    cart: [
+        { product: "bread", quantity: 5 },
+        { product: "pizza", quantity: 5 },
+    ],
+    user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+
+console.log(stateClone);
+console.log(stateDeepClone);
