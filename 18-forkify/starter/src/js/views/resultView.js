5993d7cfd8798d608cfbdb6f1dd2a228d1742396
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-17 14:35:58 +0000

    search results implementation

---
+++ 18-forkify/starter/src/js/views/resultView.js
@@ -0,0 +1,35 @@
+import View from "./View";
+import icons from "url:../../img/icons.svg";
+
+class ResultView extends View {
+    _parentElement = document.querySelector(".results");
+    _errorMessage = "No recipe found! try searching again";
+    _message = "";
+
+    _generateMarker() {
+        return this._data.map(this.#generateMarkerPreview).join("");
+    }
+
+    #generateMarkerPreview(result) {
+        return `
+            <li class="preview">
+                <a class="preview__link preview__link--active" href="#${result.id}">
+                    <figure class="preview__fig">
+                        <img src="${result.image}" alt="${result.title}" />
+                    </figure>
+                    <div class="preview__data">
+                        <h4 class="preview__title">${result.title}</h4>
+                        <p class="preview__publisher">${result.publisher}</p>
+                        <div class="preview__user-generated">
+                            <svg>
+                                <use href="${icons}.svg#icon-user"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        `;
+    }
+}
+
+export default new ResultView();
